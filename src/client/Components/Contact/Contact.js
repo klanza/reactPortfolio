f8fcab0f17c9860e3e092d9ff471b196385752d6
klanza <klanza91@gmail.com>
2018-12-20 23:17:14 -0700

    Placeholder contact section information

---
+++ src/client/Components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+
+    render() {
+        return (
+            <div>
+                <div>List of opportunities I am interested in</div>
+                <div>Input boxes, form data, and submit buttons will go here.</div>
+            </div>
+        )
+    }
+}
+
+export default Contact;
