881abefe6a9bafa05ee209628bd24c459f607adf
klanza <klanza91@gmail.com>
2018-12-20 23:20:18 -0700

    Initial footer information

---
+++ src/client/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <div>
+            <div>Footer with some standard information, play with CSS/other ideas</div>
+        </div>
+    )
+}
+
+export default Footer;
